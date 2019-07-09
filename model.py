be238e714dfd3c78757042b07d20faeae6c0cb66
liqiushi1991 <ql440@nyu.edu>
2019-07-08 23:26:16 -0400

    Update to the presentation version

--- model.py
+++ model.py
@@ -1,7 +1,14 @@
 import torch
-import torch.nn as nn
+
+import numpy              as np
+import torch.nn           as nn
 import torchvision.models as models
-from torch.nn import init
+
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return -lim, lim
 
 
 class Encoder(nn.Module):
@@ -20,13 +27,20 @@ class Encoder(nn.Module):
         else:
             raise NotImplementedError
         self.resnet = nn.Sequential(*modules)
+        self.dim = self.dim // 2
+        self.affine_W = nn.Linear(self.dim * 2, self.dim)
+        self.reset_parameters()
 
     def forward(self, images):
         """Extract the image feature vectors."""
-        features = self.resnet(images)  # [batch, self.dim, 7, 7]
+        features = self.resnet(images)  # [batch, self.dim, 8, 8]
         features = features.permute(0, 2, 3, 1)
-        features = features.view(features.size(0), -1, features.size(-1))  # [batch, 49, self.dim=2048]
+        features = features.view(features.size(0), -1, features.size(-1))  # [batch, 64, self.dim=2048]
+        features = self.affine_W(features)
         return features
+
+    def reset_parameters(self):
+        self.affine_W.weight.data.uniform_(*hidden_init(self.affine_W))
 
 
 class Attention(nn.Module):
@@ -37,18 +51,19 @@ class Attention(nn.Module):
         self.affine_U = nn.Linear(hidden_size, hidden_size)
 
         self.affine_V = nn.Linear(hidden_size, 1)
+        self.reset_parameters()
 
-    def init_weights(self):
-        init.xavier_uniform(self.affine_W.weight)
-        init.xavier_uniform(self.affine_U.weight)
-        init.xavier_uniform(self.affine_V.weight)
+    def reset_parameters(self):
+        self.affine_W.weight.data.uniform_(*hidden_init(self.affine_W))
+        self.affine_U.weight.data.uniform_(*hidden_init(self.affine_U))
+        self.affine_V.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, a, prev_hidden_state):
         att = torch.tanh(self.affine_W(a) + self.affine_U(prev_hidden_state).unsqueeze(1))  # [batch, 49, 1]
         e_t = self.affine_V(att).squeeze(2)
 
-        alpha_t = nn.Softmax(1)(e_t)  # [batch, 49]
-        context_t = (a * alpha_t.unsqueeze(2)).sum(1)  # [batch, 2048]
+        alpha_t = nn.Softmax(1)(e_t)  # [batch, 64]
+        context_t = (a * alpha_t.unsqueeze(2)).sum(1)  # [batch, 1024]
 
         return context_t, alpha_t
 
@@ -68,6 +83,15 @@ class Decoder(nn.Module):
         self.f_beta = nn.Linear(hidden_size, encoder_dim)
         self.lstm = nn.LSTMCell(hidden_size + encoder_dim, hidden_size)
         self.output_W = nn.Linear(hidden_size, vocab_size)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.embedding.weight.data.uniform_(-3e-3, 3e-3)
+        self.init_affine_h.weight.data.uniform_(*hidden_init(self.init_affine_h))
+        self.init_affine_c.weight.data.uniform_(*hidden_init(self.init_affine_c))
+        self.f_beta.weight.data.uniform_(*hidden_init(self.f_beta))
+        self.output_W.weight.data.uniform_(*hidden_init(self.output_W))
 
     def forward(self, features, captions):
         batch_size = features.size(0)
@@ -103,71 +127,3 @@ class Decoder(nn.Module):
                 prev_word = captions[:, t]
                 embedding = self.embedding(prev_word)
         return pred_words, alphas
-
-    def caption(self, features, beam_size):
-        '''
-        From https://github.com/kelvinxu/arctic-captions/blob/master/generate_caps.py
-        '''
-
-        prev_words = torch.zeros(beam_size, 1).long()
-
-        sentences = prev_words
-        top_preds = torch.zeros(beam_size, 1)
-        alphas = torch.ones(beam_size, 1, features.size(1))
-
-        completed_sentences = []
-        completed_sentences_alphas = []
-        completed_sentences_preds = []
-
-        step = 1
-        features_avg = features.mean(dim=1)
-        h = torch.tanh(self.init_affine_h(features_avg))
-        c = torch.tanh(self.init_affine_c(features_avg))
-
-        while True:
-            embedding = self.embedding(prev_words).squeeze(1)
-            context, alpha = self.attention(img_features, h)
-            gate = torch.sigmoid(self.f_beta(h))
-            gated_context = gate * context
-
-            lstm_input = torch.cat((embedding, gated_context), dim=1)
-            h, c = self.lstm(lstm_input, (h, c))
-            output = top_preds.expand_as(output) + output
-
-            if step == 1:
-                top_preds, top_words = output[0].topk(beam_size, 0, True, True)
-            else:
-                top_preds, top_words = output.view(-1).topk(beam_size, 0, True, True)
-            prev_word_idxs = top_words / output.size(1)
-            next_word_idxs = top_words % output.size(1)
-
-            sentences = torch.cat((sentences[prev_word_idxs], next_word_idxs.unsqueeze(1)), dim=1)
-            alphas = torch.cat((alphas[prev_word_idxs], alpha[prev_word_idxs].unsqueeze(1)), dim=1)
-
-            incomplete = [idx for idx, next_word in enumerate(next_word_idxs) if next_word != 1]
-            complete = list(set(range(len(next_word_idxs))) - set(incomplete))
-
-            if len(complete) > 0:
-                completed_sentences.extend(sentences[complete].tolist())
-                completed_sentences_alphas.extend(alphas[complete].tolist())
-                completed_sentences_preds.extend(top_preds[complete])
-            beam_size -= len(complete)
-
-            if beam_size == 0:
-                break
-            sentences = sentences[incomplete]
-            alphas = alphas[incomplete]
-            h = h[prev_word_idxs[incomplete]]
-            c = c[prev_word_idxs[incomplete]]
-            features = features[prev_word_idxs[incomplete]]
-            top_preds = top_preds[incomplete].unsqueeze(1)
-            prev_words = next_word_idxs[incomplete].unsqueeze(1)
-
-            if step > 50:
-                break
-            step += 1
-
-        idx = completed_sentences_preds.index(max(completed_sentences_preds))
-        sentence = completed_sentences[idx]
-        alpha = completed_sentences_alphas[idx]
-        return sentence, alpha
